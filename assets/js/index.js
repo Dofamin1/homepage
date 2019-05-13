167d0e2ce1e209a5ca60a3a303fa5c3dcdf78290
Dofamin1 <gugenot990@gmail.com>
2019-05-13 13:34:12 +0300

    create new function - checkDateParams(checks passed to checkDateParams dates)

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,7 @@ const getDateDifference = ( timeBefore, timeAfter ) => {;
     let yearDif = timeAfter.year - timeBefore.year;
     let monthDif = timeAfter.month - timeBefore.month;
     let dayDif = timeAfter.day - timeBefore.day;
+    checkDateParams(yearDif, monthDif, dayDif)
     if(monthDif < 0) {
         yearDif = 0;
         monthDif = (12 - timeBefore.month) + timeAfter.month
@@ -26,10 +27,20 @@ const getDateDifference = ( timeBefore, timeAfter ) => {;
         dayDif
     }
 }
+
 const getDayCountOfMonth = (year, month) => {
     const date = new Date(year, month + 1, 0);
     return date.getDate();
 }
 
+const checkDateParams = (yearDif, monthDif, dayDif) => {
+    if(yearDif < 0) throw new Error("second date is earlier than first one"); 
+    else if(yearDif == 0 && monthDif < 0) throw new Error("second date is earlier than first one");
+    else if(yearDif == 0 && monthDif == 0 && dayDif < 0) throw new Error("second date is earlier than first one");
+    else if(yearDif == 0 && monthDif == 0 && dayDif == 0) throw new Error("dates are the same");
+    
+}
+const dateOfStartPresidency = getDate("2017-01-20");
+const currentDay = getDate();
 
 
