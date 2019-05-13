7a90a5075e6ee48de517fea34422adeff5f0d97f
Dofamin1 <gugenot990@gmail.com>
2019-05-13 13:31:08 +0300

    create getDateDifference function (returns difference of two passed dates)

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,5 +7,29 @@ const getDate = (timePoint = null) => {
     }
 }
 
+const getDateDifference = ( timeBefore, timeAfter ) => {;
+    let yearDif = timeAfter.year - timeBefore.year;
+    let monthDif = timeAfter.month - timeBefore.month;
+    let dayDif = timeAfter.day - timeBefore.day;
+    if(monthDif < 0) {
+        yearDif = 0;
+        monthDif = (12 - timeBefore.month) + timeAfter.month
+    }
+
+    if(dayDif < 0) {
+        monthDif -= 1;
+        dayDif = (getDayCountOfMonth(timeBefore.year, timeBefore.month) - timeBefore.day) + timeAfter.day
+    }
+    return {
+        yearDif, 
+        monthDif, 
+        dayDif
+    }
+}
+const getDayCountOfMonth = (year, month) => {
+    const date = new Date(year, month + 1, 0);
+    return date.getDate();
+}
+
 
 
